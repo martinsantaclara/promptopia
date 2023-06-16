d1b1e6c3d96fbc6e5f551e00a391007fb6cedc90
Martin <martinsantaclara@gmail.com>
2023-06-16 09:17:07 -0300

    not found

--- app/users/[id]/not-Found.tsx
+++ app/users/[id]/not-Found.tsx
@@ -1,6 +1,8 @@
 const NotFound = () => {
     return (
-        <h1 style={{fontSize: '4rem', fontWeight: '900'}}>User Not Found!!!</h1>
+        <h1 style={{fontSize: '4rem', fontWeight: '900'}}>
+            User Not Found - Back to the Home!!!
+        </h1>
     );
 };
 
